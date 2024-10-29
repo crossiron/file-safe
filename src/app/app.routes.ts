33224c5d49a52038d8a20877dbb331523c4c7864
crossiron <rodfrlopes@gmail.com>
2024-10-29 20:54:01 +0000

    prepared app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
-import { Routes } from '@angular/router';
+import {Router, Routes} from '@angular/router';
+import {isLoggedInGuard, isNotLoggedInGuard} from './guards/is-logged-in.guard';
 
-export const routes: Routes = [];
+export const ROUTE_SEGMENT_LOGIN = ['/login'];
+export const ROUTE_SEGMENT_SAFE = ['/safe'];
+
+export const navigateToLogin = (router: Router) => router.navigate(ROUTE_SEGMENT_LOGIN);
+export const navigateToSafe = (router: Router) => router.navigate(ROUTE_SEGMENT_SAFE);
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    canActivate: [isNotLoggedInGuard(navigateToLogin)],
+    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent),
+  },
+  {
+    path: 'safe',
+    canActivate: [isLoggedInGuard(navigateToSafe)],
+    loadChildren: () => import('./pages/safe/safe.routes')
+  },
+  {
+    path: '**',
+    redirectTo: '/login'
+  }
+];
