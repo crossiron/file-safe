d10999f3668db485589cfc3ba058beefe9a92fa0
crossiron <rodfrlopes@gmail.com>
2024-10-30 19:11:27 +0000

    reviewed logic behind session

--- src/app/pages/safe/safe.component.ts
+++ src/app/pages/safe/safe.component.ts
@@ -8,21 +8,6 @@ import {SessionService} from '../../services/session.service';
   templateUrl: './safe.component.html',
   styleUrl: './safe.component.scss'
 })
-export class SafeComponent implements OnInit, OnDestroy {
-  #renderer = inject(Renderer2);
-  #session = inject(SessionService);
-  #unlisten?: () => void;
+export class SafeComponent {
 
-  constructor() {
-  }
-
-  ngOnInit(): void {
-    this.#unlisten = this.#renderer.listen('window', 'click', _ => this.#session.extend());
-  }
-
-  ngOnDestroy(): void {
-    if (this.#unlisten) {
-      this.#unlisten();
-    }
-  }
 }
--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -1,48 +1,52 @@
-import {computed, inject, Injectable} from '@angular/core';
+import {computed, inject, Injectable, Renderer2} from '@angular/core';
 import {environment} from '../../environments/environment';
 import {SessionStorageService} from './session-storage.service';
 import {navigateToHome, navigateToLogin} from '../app.routes';
 import {Router} from '@angular/router';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class SessionService {
-    #storage = inject(SessionStorageService);
-    #router = inject(Router);
-    #timeoutID?: number;
+  #storage = inject(SessionStorageService);
+  #router = inject(Router);
+  #renderer = inject(Renderer2);
 
-    isLoggedIn = computed(() => {
-        const lastActivity = this.#storage.lastActivity();
-        return lastActivity !== SessionStorageService.LAST_ACTIVITY_INITIAL && (Date.now() - lastActivity) < environment.sessionTimeout
-    });
+  #selfLogoutTimerID?: number;
+  #disposeClickHandler?: () => void;
 
-    extend(): void {
-        if (this.isLoggedIn()) {
-            this.#schedule(Date.now());
-        } else {
-            this.logout();
-        }
+  #registerLastActivity(time: number) {
+    clearTimeout(this.#selfLogoutTimerID);
+    if(time === SessionStorageService.LAST_ACTIVITY_INITIAL) {
+      this.#selfLogoutTimerID = undefined;
+    } else {
+      this.#selfLogoutTimerID = window.setTimeout(() => this.logout(), environment.sessionTimeout);
     }
+    this.#storage.lastActivity.set(time);
+  }
 
-    #schedule(time: number) {
-        clearTimeout(this.#timeoutID);
-        this.#timeoutID = window.setTimeout(() => {
-            this.logout();
-        }, environment.sessionTimeout);
-        this.#storage.lastActivity.set(time);
+  isLoggedIn = computed(() => {
+    const lastActivity = this.#storage.lastActivity();
+    return lastActivity !== SessionStorageService.LAST_ACTIVITY_INITIAL && (Date.now() - lastActivity) < environment.sessionTimeout
+  });
+
+  extend(): void {
+    if (this.isLoggedIn()) {
+      this.#registerLastActivity(Date.now());
+    } else {
+      this.logout();
     }
+  }
 
-    login(): void {
-        this.#schedule(Date.now());
-        navigateToHome(this.#router);
-    }
+  login(): void {
+    this.#registerLastActivity(Date.now());
+    this.#disposeClickHandler = this.#renderer.listen('window', 'click', _ => this.extend());
+    navigateToHome(this.#router);
+  }
 
-    logout(): void {
-        clearTimeout(this.#timeoutID);
-        this.#timeoutID = undefined;
-
-        this.#storage.lastActivity.set(SessionStorageService.LAST_ACTIVITY_INITIAL);
-        navigateToLogin(this.#router);
-    }
+  logout(): void {
+    this.#registerLastActivity(SessionStorageService.LAST_ACTIVITY_INITIAL);
+    this.#disposeClickHandler?.();
+    navigateToLogin(this.#router);
+  }
 }
